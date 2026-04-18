e210b8cffe6c15b6f659bb87203d4cb8bfc67a0d
elndower <51922927+EngenhariaAc@users.noreply.github.com>
2026-04-18 19:48:42 +0700

    Create Launch.java

---
+++ Launch.java
@@ -0,0 +1,8 @@
+public class Launch {
+    public static void countdown() {
+        for (int i = 5; i >= 0; i--) {
+            System.out.println("T-minus " + i);
+        }
+        System.out.println("Liftoff! 🚀");
+    }
+}
